25fdb9ca02c9a92e5bbd51fd15197acbd13ae106
Rahul Jangid <39289586+rahuljangid236@users.noreply.github.com>
2022-10-05 19:28:57 +0530

    Create String_Typecast.java

---
+++ 3. Basics of Programming, Programming - [Syntax], Input, Output - Post Class/String_Typecast.java
@@ -0,0 +1,38 @@
+/* Author = Rahul Jangid */
+/* GitHub Id = rahuljangid236 */
+/* Language = JAVA */
+/* Address = Udaipur,Rajasthan,INDIA (313001)*************************************************************************/
+
+/********************************************************************************************************************* 
+String Typecast
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given an integer N, you need to typecast this integer to String. If the typecasting is done successfully then we will print "Nice Job" otherwise "Wrong answer".
+Input
+User task:
+Since this is a functional problem you don't have to worry about the input. You just have to complete the function checkConvertion(), which contains N as a parameter.
+Output
+You need to return the typecasted string value. The driver code will print "Nice Job" otherwise "Wrong answer".
+Example
+Sample Input:
+5
+
+Sample Output:
+Nice Job
+
+Sample Input:
+6
+
+Sample Output:
+Nice Job
+ **********************************************************************************************************************/
+
+/*Solution */
+
+static String checkConevrtion(int a)
+ {
+       // your code here
+       return String.valueOf(a);
+    }
