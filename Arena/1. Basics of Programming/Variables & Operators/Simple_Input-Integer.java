9b12996e551bbd345911d89cade3091da1776e09
Rahul Jangid <39289586+rahuljangid236@users.noreply.github.com>
2022-10-13 00:10:53 +0530

    Add files via upload

---
+++ Arena/1. Basics of Programming/Variables & Operators/Simple_Input-Integer.java
@@ -0,0 +1,51 @@
+/* Author = Rahul Jangid */
+/* GitHub Id = rahuljangid236 */
+/* Language = JAVA */
+/* Address = Udaipur,Rajasthan,INDIA (313001)*************************************************************************/
+
+/********************************************************************************************************************* 
+Simple Input - Integer
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given an integer N, your task is to take input of the given integer and print it.
+Input
+You don't have to worry about the input, you just have to complete the function printIntger()
+
+Constraints:-
+1 <= N <= 100
+Output
+Print the integer N.
+Example
+Sample Input:-
+3
+
+Sample Output:
+3
+
+Sample Input:
+56
+
+Sample Output:
+56
+ **********************************************************************************************************************/
+
+/*Solution */
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+                      Scanner sc = new Scanner(System.in);
+                      int N = sc.nextInt();
+                      System.out.println(N);
+    }
+}
